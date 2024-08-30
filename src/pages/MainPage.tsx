67fc4ed4fee3da6a7fd94b07f9627ab9679e43b5
MarioGrusz <mario.gruszczynski@gmail.com>
2024-08-30 14:53:31 +0200

    portfoliov4 final touches

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import "./style.scss";
 import { useRef, useEffect } from "react";
-import { SmoothScroll } from "./smoothScroll";
+//import { SmoothScroll } from "./smoothScroll";
 import Hero from "../components/Hero";
 import SlideTabs from "../components/SlideTabs";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
 import Footer from "../components/Footer";
 import Marquee from "../components/Marquee";
+
+import Lenis from "lenis";
 
 const MainPage: React.FC = () => {
   const scrollableRef = useRef<HTMLDivElement>(null);
@@ -16,6 +18,38 @@ const MainPage: React.FC = () => {
   const aboutSectionRef = useRef<HTMLDivElement>(null);
   const projectsSectionRef = useRef<HTMLDivElement>(null);
   const contactSectionRef = useRef<HTMLDivElement>(null);
+
+  let lenis: any;
+  //const refs = useRef<HTMLElement[]>([]);
+
+  const initSmoothScrolling = () => {
+    if (lenis) lenis.destroy();
+    lenis = new Lenis({
+      lerp: 0.2,
+      smoothWheel: true,
+    });
+    const scrollFn = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(scrollFn);
+    };
+    requestAnimationFrame(scrollFn);
+  };
+
+  const init = () => {
+    initSmoothScrolling();
+    scroll();
+  };
+
+  useEffect(() => {
+    const triggerResize = () => {
+      window.dispatchEvent(new Event("resize"));
+    };
+    const initAll = () => {
+      init();
+      triggerResize(); // Force a resize event after everything is initialized
+    };
+    initAll();
+  }, []);
 
   const content = (
     <>
@@ -29,17 +63,17 @@ const MainPage: React.FC = () => {
     </>
   );
 
-  useEffect(() => {
-    if (scrollableRef?.current) {
-      if (scrollableRef?.current && imageRefs?.current) {
-        const filteredImages = imageRefs?.current.filter(
-          (img): img is HTMLImageElement => img !== null
-        );
-        if (filteredImages)
-          new SmoothScroll(scrollableRef.current, filteredImages as any);
-      }
-    }
-  }, [scrollableRef]);
+  // useEffect(() => {
+  //   if (scrollableRef?.current) {
+  //     if (scrollableRef?.current && imageRefs?.current) {
+  //       const filteredImages = imageRefs?.current.filter(
+  //         (img): img is HTMLImageElement => img !== null
+  //       );
+  //       if (filteredImages)
+  //         new SmoothScroll(scrollableRef.current, filteredImages as any);
+  //     }
+  //   }
+  // }, [scrollableRef]);
 
   return (
     <main className="main-container">
