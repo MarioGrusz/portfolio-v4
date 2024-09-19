2dcebe008daf27d681366511b4bcb55572b6cacf
MarioGrusz <mario.gruszczynski@gmail.com>
2024-09-19 23:20:41 +0200

    more changes SEO and performance improvments

--- src/App.tsx
+++ src/App.tsx
@@ -69,9 +69,9 @@ const App = () => {
 
         {/* Load Google Fonts asynchronously */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Libre+Franklin:ital,wght@0,100..900;1,100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
-        />
+        ></link>
 
         {/* Basic SEO */}
         <title>Mariusz Gruszczynski • Software Developer</title>
@@ -143,6 +143,7 @@ const App = () => {
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-STH3BH5TKQ"
+          rel="preload"
         ></script>
         <script>
           {`
