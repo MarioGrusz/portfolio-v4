40051028e093a5fb232a7269882697835a5131dc
MarioGrusz <mario.gruszczynski@gmail.com>
2024-10-04 00:08:01 +0200

    small smooth scroll improvment

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,18 +1,25 @@
 import "./style.scss";
-import { useRef, Suspense, lazy } from "react";
+import { useRef } from "react";
 import { useSmoothScrolling } from "../hooks/useSmoothScroll";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+import Hero from "../components/Hero";
+import SlideTabs from "../components/SlideTabs";
+import AboutMe from "../components/AboutMe";
+import Projects from "../components/Projects";
+import Footer from "../components/Footer";
+import Marquee from "../components/Marquee";
+
 gsap.registerPlugin(ScrollTrigger);
 
-// Lazy load components
-const Hero = lazy(() => import("../components/Hero"));
-const SlideTabs = lazy(() => import("../components/SlideTabs"));
-const AboutMe = lazy(() => import("../components/AboutMe"));
-const Projects = lazy(() => import("../components/Projects"));
-const Footer = lazy(() => import("../components/Footer"));
-const Marquee = lazy(() => import("../components/Marquee"));
+// // Lazy load components
+// const Hero = lazy(() => import("../components/Hero"));
+// const SlideTabs = lazy(() => import("../components/SlideTabs"));
+// const AboutMe = lazy(() => import("../components/AboutMe"));
+// const Projects = lazy(() => import("../components/Projects"));
+// const Footer = lazy(() => import("../components/Footer"));
+// const Marquee = lazy(() => import("../components/Marquee"));
 
 const MainPage: React.FC = () => {
   const scrollableRef = useRef<HTMLDivElement>(null);
@@ -38,23 +45,21 @@ const MainPage: React.FC = () => {
   return (
     <main className="main-container">
       <div className="scrollable" ref={scrollableRef}>
-        <Suspense>
-          <SlideTabs
-            contactSectionRef={contactSectionRef}
-            aboutSectionRef={aboutSectionRef}
-            projectsSectionRef={projectsSectionRef}
-            topSectionRef={topSectionRef}
-          />
-          <Hero />
-          <AboutMe aboutSectionRef={aboutSectionRef} />
-          <Marquee content={content} />
-          <Marquee content={content} isReverse={true} />
-          <Projects projectsSectionRef={projectsSectionRef} />
-          <Footer
-            contactSectionRef={contactSectionRef}
-            topSectionRef={topSectionRef}
-          />
-        </Suspense>
+        <SlideTabs
+          contactSectionRef={contactSectionRef}
+          aboutSectionRef={aboutSectionRef}
+          projectsSectionRef={projectsSectionRef}
+          topSectionRef={topSectionRef}
+        />
+        <Hero />
+        <AboutMe aboutSectionRef={aboutSectionRef} />
+        <Marquee content={content} />
+        <Marquee content={content} isReverse={true} />
+        <Projects projectsSectionRef={projectsSectionRef} />
+        <Footer
+          contactSectionRef={contactSectionRef}
+          topSectionRef={topSectionRef}
+        />
       </div>
     </main>
   );
